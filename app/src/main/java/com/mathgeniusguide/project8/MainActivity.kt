ee9c66152ace65f5a88c79633970c89be96a23b5
Jonathan Fitch <jfitch1@ymail.com>
2019-10-20 14:41:29 -0400

    firebase signin almost complete

--- app/src/main/java/com/mathgeniusguide/project8/MainActivity.kt
+++ app/src/main/java/com/mathgeniusguide/project8/MainActivity.kt
@@ -2,8 +2,10 @@ package com.mathgeniusguide.project8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.navigation.NavController
@@ -12,10 +14,29 @@ import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
 
-class MainActivity : AppCompatActivity() {
+
+
+class MainActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener  {
     lateinit var navController: NavController
+    private val TAG = "Go4Lunch"
+    private val RC_SIGN_IN = 9001
+    private val ANONYMOUS = "anonymous"
+
+    // Firebase variables
+    lateinit var googleApiClient: GoogleApiClient
+    lateinit var firebaseAuth: FirebaseAuth
+    var firebaseUser: FirebaseUser? = null;
+    var username = ANONYMOUS
+    var photoUrl = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +51,30 @@ class MainActivity : AppCompatActivity() {
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
-        nav_view.setNavigationItemSelectedListener {
+        drawer_view.setNavigationItemSelectedListener {
             it.onNavDestinationSelected(navController)
+        }
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleApiClient = GoogleApiClient.Builder(this)
+            .enableAutoManage(this, this)
+            .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+            .build();
+
+        // Initialize FirebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser()
+        if (firebaseUser != null) {
+            username = firebaseUser!!.getDisplayName()?: ANONYMOUS
+            if (firebaseUser!!.getPhotoUrl() != null) {
+                photoUrl = firebaseUser!!.getPhotoUrl().toString()
+            }
+            findNavController(nav_host_fragment).navigate(R.id.action_login)
         }
     }
 
@@ -49,6 +92,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            R.id.logout -> logout()
+            else -> item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun logout(): Boolean {
+        firebaseAuth.signOut()
+        Auth.GoogleSignInApi.signOut(googleApiClient);
+        username = ANONYMOUS;
+        firebaseUser = null
+        photoUrl = ""
+        findNavController(nav_host_fragment).navigate(R.id.action_logout)
+        return true
+    }
+
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not be available.
+        Log.d(TAG, "onConnectionFailed:$connectionResult")
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/mathgeniusguide/project8/fragments/Login.kt
+++ app/src/main/java/com/mathgeniusguide/project8/fragments/Login.kt
@@ -5,11 +5,87 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.firebase.auth.FirebaseAuth
 import com.mathgeniusguide.project8.R
+import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import android.util.Log
+import androidx.fragment.app.FragmentActivity
+import kotlinx.android.synthetic.main.login.*
+import com.mathgeniusguide.project8.MainActivity
+import android.content.Intent
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.Task
+import androidx.annotation.NonNull
+import androidx.navigation.fragment.findNavController
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.AuthCredential
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInResult
 
-class Login: Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+class Login : Fragment() {
+    private val TAG = "Go4Lunch"
+    private val RC_SIGN_IN = 9001
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // enable Google Sign In Button
+        googleButton.setOnClickListener {
+            startActivityForResult(
+                Auth.GoogleSignInApi.getSignInIntent((activity as MainActivity).googleApiClient),
+                RC_SIGN_IN
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result.isSuccess) {
+                // Google Sign-In was successful, authenticate with Firebase
+                firebaseAuthWithGoogle(result.signInAccount!!)
+            } else {
+                // Google Sign-In failed
+                Log.e(TAG, "Google Sign-In failed.")
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.d(TAG, "firebaseAuthWithGooogle:" + acct.id!!)
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        (activity as MainActivity).firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(activity!!, { task ->
+                Log.d(TAG, "signInWithCredential:onComplete:${task.isSuccessful}")
+
+                // If sign in fails, display a message to the user. If sign in succeeds
+                // the auth state listener will be notified and logic to handle the
+                // signed in user can be handled in the listener.
+                if (!task.isSuccessful) {
+                    Log.w(TAG, "signInWithCredential", task.exception)
+                    Toast.makeText(
+                        context, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    findNavController().navigate(R.id.action_login)
+                }
+            })
     }
 }
